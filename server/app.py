f05e46521d4f13ccc220f292f6c5f3f4281cf26a
brenda njoki <ngunjiribrenda5@gmail.com>
2025-09-09 18:15:18 +0300

    Initial commit

--- server/app.py
+++ server/app.py
@@ -3,3 +3,17 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+#decorator-functions that take functions as arguments and return them decorated with new features.
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+#views -functions that map to URLs.
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__== '__main__':
+    app.run(port=5555, debug=True)
